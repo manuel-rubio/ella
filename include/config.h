b2f318489abc1cbebdfd48e8569c3699d08fc43b
Manuel Rubio <manuel@altenwald.com>
2008-05-11 00:58:38 +0000

    datos para consola y versión.

--- include/config.h
+++ include/config.h
@@ -1,5 +1,7 @@
 /* -*- mode:C; coding:utf-8 -*- */
 
+#define EWS_VERSION         0.1
+
 #define __CONFIG_STATIC
 #define __CONFIG_DIR        "etc/ews"
 #define __CONFIG_FILE       "etc/ews/method.ini"
@@ -7,3 +9,5 @@
 // #define __EWS_SIMPLE_USER
 
 #define __MODULES_DIR       "modules"
+
+#define __CONSOLE_SOCKET    "ews.ctl"
