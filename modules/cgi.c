dfe1bcefe9cd65f7ce6e4cba853f8be5062d046e
Manuel Rubio <manuel@altenwald.com>
2008-06-23 17:54:23 +0000

    agregadas más variables de entorno para scripts CGI.

--- modules/cgi.c
+++ modules/cgi.c
@@ -12,16 +12,31 @@ int cgi_run( struct Bind_Request *br, responseHTTP *rs ) {
     requestHTTP *rh = br->request;
     virtualHost *vh = NULL;
     hostLocation *hl = NULL;
-    char *host_name = ews_get_header_value(rh, "Host", 0);
+    char *host = ews_get_header_value(rh, "Host", 0);
+    char host_name[80];
+    char remote_port[10];
+    char *port = NULL; // port 80 by default
     char *path = NULL;
     char *cgi = NULL;
     char buffer[BUFFER_SIZE] = { 0 };
+    char tmp_exec[BUFFER_SIZE] = { 0 };
     char uri[BUFFER_SIZE] = { 0 };
     char *content = NULL, *aux = NULL, *get = NULL;
+    char *admin = NULL;
     FILE *cmd = NULL;
     int size = 0, len = 0, ptr = 0, f, i;
     struct stat st;
 
+    if (host != NULL) {
+        strcpy(host_name, host);
+        for (i=0; host_name[i]!='\0'; i++) {
+            if (host_name[i] == ':') {
+                host_name[i] = '\0';
+                port = host_name + i + 1;
+                break;
+            }
+        }
+    }
     if (host_name != NULL) {
         vh = ews_connector_find_vhost((virtualHost *)br->bc->vhosts, host_name);
     }
@@ -32,6 +47,7 @@ int cgi_run( struct Bind_Request *br, responseHTTP *rs ) {
     hl = ews_connector_find_location(vh->locations, rh->uri);
     cgi = ews_get_detail_value(hl->details, "cgi", 0);
     path = ews_get_detail_value(hl->details, "path", 0);
+    admin = ews_get_detail_value(hl->details, "admin", 0);
 
     if (cgi != NULL && strcmp(cgi, "on") == 0) {
         sprintf(buffer, "%s/%s", path, rh->uri + (strlen(hl->base_uri)) + 1);
@@ -57,6 +73,7 @@ int cgi_run( struct Bind_Request *br, responseHTTP *rs ) {
             ews_verbose(LOG_LEVEL_ERROR, "execute permission isn't set to this file [%s]", buffer);
             rs->code = 500;
         } else {
+            sprintf(remote_port, "%d", (br->client).sin_port);
             // TODO: set environment vars
             setenv("TERM", "dumb", 1);
             setenv("SCRIPT_NAME", uri, 1);
@@ -65,19 +82,29 @@ int cgi_run( struct Bind_Request *br, responseHTTP *rs ) {
             setenv("GATEWAY_INTEFACE", GATEWAY_VER, 1);
             // FIXME: Accept must be all values concatenated (some value, -1, to get all?)
             setenv("HTTP_ACCEPT", ews_get_header_value(rh, "Accept", 0), 1);
-            setenv("HTTP_HOST", host_name, 1);
+            setenv("HTTP_HOST", host, 1);
             setenv("HTTP_USER_AGENT", ews_get_header_value(rh, "User-Agent", 0), 1);
-            // setenv("REMOTE_ADDR", xxx, 1);
-            // setenv("REMOTE_PORT", xxx, 1);
-            // setenv("REQUEST_METHOD", xxx, 1);
-            // setenv("REQUEST_URI", rh->uri, 1);
-            // setenv("SERVER_ADDR", xxx, 1);
-            // setenv("SERVER_ADMIN", xxx, 1);
-            // setenv("SERVER_NAME", xxx, 1);
-            // setenv("SERVER_PROTOCOL", xxx, 1);
+            setenv("REMOTE_ADDR", inet_ntoa((br->client).sin_addr), 1);
+            setenv("REMOTE_PORT", remote_port, 1);
+            setenv("REQUEST_METHOD", rh->request, 1);
+            setenv("REQUEST_URI", rh->uri, 1);
+            // FIXME: server 0.0.0.0 isn't correct value, this should be 127.0.0.1, 192.168.73.24...
+            setenv("SERVER_ADDR", br->bc->host, 1);
+            setenv("SERVER_ADMIN", (admin == NULL) ? "webmaster@localhost" : admin, 1);
+            setenv("SERVER_NAME", host_name, 1);
+            setenv("SERVER_PORT", port, 1);
+            setenv("SERVER_PROTOCOL", rh->version, 1);
             setenv("SERVER_SIGNATURE", PACKAGE_NAME "/" PACKAGE_VERSION, 1);
             setenv("SERVER_SOFTWARE", PACKAGE_NAME "/" PACKAGE_VERSION, 1);
+            setenv("QUERY_STRING", (get == NULL) ? "" : get, 1);
 
+            if (rh->content != NULL) {
+                // FIXME: doesn't work, try to another thing to send POST data to CGI
+                sprintf(tmp_exec, "echo '%s' | %s", rh->content, buffer);
+                ews_verbose(LOG_LEVEL_DEBUG, "send to exec: %s", tmp_exec);
+            } else {
+                strcpy(tmp_exec, buffer);
+            }
             cmd = popen(buffer, "r");
             if (cmd) {
                 content = (char *)ews_malloc(BUFFER_SIZE);
@@ -96,13 +123,20 @@ int cgi_run( struct Bind_Request *br, responseHTTP *rs ) {
                 }
                 content[ptr] = '\0';
                 pclose(cmd);
-                ews_free(rs->content, "cgi_run");
-                rs->content_type = HEADER_CONTENT_RAW;
-                rs->content = content;
-                rs->code = 200;
-                strcpy(rs->message, "OK");
-                strcpy(rs->version, "1.0");
-                ews_add_header(&rs->headers, "Server", "ews/0.1", 0);
+                if (ptr == 0) {
+                    ews_free(content, "cgi_run");
+                    rs->code = 500;
+                } else {
+                    ews_free(rs->content, "cgi_run");
+                    rs->content_type = HEADER_CONTENT_RAW;
+                    rs->content = content;
+                    rs->code = 200;
+                    strcpy(rs->message, "OK");
+                    strcpy(rs->version, "1.0");
+                    ews_add_header(&rs->headers, "Server", "ews/0.1", 0);
+                }
+            } else {
+                rs->code = 500;
             }
         }
     }
