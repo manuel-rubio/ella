e7e53f6a1e9e274061ae5e77f17d59b606ccc493
Manuel Rubio <manuel@altenwald.com>
2009-07-15 16:25:47 +0000

    multiplatform compiling in release 0.1

--- tools/ews_copy.sh
+++ tools/ews_copy.sh
@@ -9,7 +9,7 @@ DEST=../tags/ews-$1
 
 cd $(dirname $0)/..
 
-DATA="common include main modules share"
+DATA="common include main modules share arch/*"
 
 cp -vf AUTHORS COPYING INSTALL Makefile.in README dist.sh configure.ac $DEST
 for i in $DATA; do
